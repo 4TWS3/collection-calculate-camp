cf20cac02bd8e404e5635c1e1f61e1ada65bfa8f
ljj <yeshrb@126.com>
2018-03-27 22:57:45 +0800

    get_union_spec passed

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -5,15 +5,12 @@ var get_integer_interval = require('./get_integer_interval.js');
 function get_letter_interval_2(number_a, number_b) {
   let intArry = get_integer_interval(number_a, number_b);
   let baseCode = 'a'.charCodeAt();
-  let offsetToCharCode(offset) {
-    if(offset % 26 === 0 )
-      return 'z'.charCodeAt();
-   return  offset % 26 + baseCode -1 ;
-  }
+
   return intArry.map(item => {
     if(item <= 26 )
-         return String.fromCharCode(item + baseCode -1)
-     return String.fromCharCode(item /26 + baseCode -1) +  String.fromCharCode(item %26 ===0 ?  + baseCode -1)
+         return String.fromCharCode(item + baseCode -1);
+     return String.fromCharCode(item %26 === 0? 'a'.charCodeAt() : item/26+ baseCode -1) +
+            String.fromCharCode(item %26 === 0? 'z'.charCodeAt() : item%26+ baseCode -1)
   });
 }
 
--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+  let ret =  collection_a;
+  collection_b.forEach(it_b => {
+    if(ret.indexOf(it_b)=== -1)
+      ret.push(it_b);
+  });
+  return ret;
 }
 
 module.exports = get_union;
