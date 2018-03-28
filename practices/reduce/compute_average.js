ec4d82588eab3bc6e2caa11c70eb1912e007bece
yeshrb@126.com <yeshrb@126.com>
2018-03-28 17:04:14 +0800

    comppute_average_spec.js pass

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function compute_average(collection) {
-  //在这里写入代码
+  if(collection.length === 0)
+    new Error('parameter canot be empty');
+ let sum = collection.reduce((pre,curr) => pre + curr);
+ return sum /collection.length;
 }
 
 module.exports = compute_average;
