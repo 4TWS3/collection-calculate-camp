c0632bdd506fa82281152509d89c663e8d26e90a
ljj <yeshrb@126.com>
2018-03-28 23:15:39 +0800

    section_1 practice_1_spec.js pass

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,11 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  return collection_a.reduce((pre,curr,index,arr) => {
+    if(collection_b.indexOf(curr) >= 0) {
+      return pre.concat(curr);
+    }
+    else
+      return pre;
+  },[])
 }
 
 module.exports = collect_same_elements;
