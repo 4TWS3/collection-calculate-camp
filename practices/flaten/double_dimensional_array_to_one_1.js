3ab487df7e6dc0136a73b32c75651ee84e0d966d
yeshrb@126.com <yeshrb@126.com>
2018-03-28 15:56:13 +0800

    double_dimensional_array_to_one_1_spec.js 通过

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,20 @@
 'use strict';
+function flattern(collection) {
+  if(collection.length === 1) {
+    return (Array.isArray(collection[0])) ? flattern(collection[0]):[collection[0]]
+  }
+  else if (!Array.isArray(collection[0])) {
+    return [collection[0]].concat(flattern(collection.slice(1)));
+  }
+  else {
+    return (flattern(collection[0])).concat(flattern(collection.slice(1)));
+  }
+}
+
 
 function double_to_one(collection) {
 
-  //在这里写入代码
+  return flattern(collection);
 }
 
 module.exports = double_to_one;
