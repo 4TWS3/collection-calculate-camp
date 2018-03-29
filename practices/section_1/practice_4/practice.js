2756cb954c262567cbc4739f05e6ccd4fc497358
yeshrb@126.com <yeshrb@126.com>
2018-03-29 11:38:25 +0800

    section_1\priactice_4_spec.js 通过

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,7 @@
+var getSameElements = require('../getSameElements');
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  let ca =collection_a.map(it => it.key);
+  return getSameElements(ca,object_b.value);
 }
 
 module.exports = collect_same_elements;
