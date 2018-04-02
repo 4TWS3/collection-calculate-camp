dc995d4f55d610cb20170875a95bee5b3f4411e2
ljj <yeshrb@126.com>
2018-04-02 22:48:13 +0800

    Refactor get_intersection_spec.js

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,14 +1,6 @@
 'use strict';
-
 function get_intersection(collection_a, collection_b) {
-  let ret = [];
- collection_b.forEach(it_b => {
-   let item = collection_a.find(it_a => {
-     return it_a === it_b;
-   });
-   if(item) ret.push(item);
- });
- return ret;
+  return collection_b.filter(it => collection_a.indexOf(it) > -1)
 }
 
 module.exports = get_intersection;
--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var get_integer_interval = require('./get_integer_interval.js');
 function get_letter_interval_2(number_a, number_b) {
   let intArry = get_integer_interval(number_a, number_b);
